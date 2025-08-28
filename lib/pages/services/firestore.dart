c3af3bade4ce68d49dae8e212acad03d9c4bcb27
Md.Jakir Hossain <jakir7z7z7z7z@gmail.com>
2025-08-28 14:12:07 +0600

    this it my first firebase project

---
+++ lib/pages/services/firestore.dart
@@ -0,0 +1,19 @@
+class FirestoreService{
+
+
+  //get collection of notes
+
+
+  //CREATE: add a new note
+
+
+  //READ : get notes from database
+
+
+  //UPDATE : update notes given a doc id
+
+
+  // DELETE :  delete notes given a doc id
+
+
+}
